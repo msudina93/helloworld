0ba20a75c75bab1645fac5e2295130cff81babd0
msudina93 <msudina@kent.edu>
2018-10-27 16:05:12 -0400

    update read me notes and added new class

--- HelloWorld/src/info/coadingZone/HelloWorld.java
+++ HelloWorld/src/info/coadingZone/HelloWorld.java
@@ -2,7 +2,7 @@ package info.coadingZone;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello Mohan sandeep Sudina");
+		System.out.println("Alternate Unniverse!");
 
 	}
 
---
+++ HelloWorld/src/info/coadingZone/example.java
@@ -0,0 +1,9 @@
+package info.coadingZone;
+
+public class example {
+	public static void main(String[] args) {
+		System.out.println("Alternate Unnivers 22222e!");
+
+	}
+
+}
