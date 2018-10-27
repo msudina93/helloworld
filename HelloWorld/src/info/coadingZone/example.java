06b579dc7ebc329ec3c3ed976c5d8d93de16fbb9
msudina93 <msudina@kent.edu>
2018-10-27 16:17:42 -0400

    changes as per req

--- HelloWorld/src/info/coadingZone/example.java
+++ HelloWorld/src/info/coadingZone/example.java
@@ -2,7 +2,7 @@ package info.coadingZone;
 
 public class example {
 	public static void main(String[] args) {
-		System.out.println("Alternate Unnivers 22222e!");
+		System.out.println("Present Unnivers 22222e!");
 		System.out.printIn("New line added here!");
 
 	}
